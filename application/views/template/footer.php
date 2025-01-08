26b3d914ae4d37e4396fce427ec66c9896a331c7
Muhammad Nurkholis <mn.kholisss@gmail.com>
2025-01-08 15:05:03 +0700

    d

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -11,36 +11,47 @@
 </footer>
 <!-- /footer content -->
 
+
 <!-- jQuery -->
-<script src="<?php echo base_url() ?>template/vendors/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap -->
-<script src="<?php echo base_url() ?>template/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- FastClick -->
-<script src="<?php echo base_url() ?>template/vendors/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/fastclick/lib/fastclick.js"></script>
 <!-- NProgress -->
-<script src="<?php echo base_url() ?>template/vendors/nprogress/nprogress.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/nprogress/nprogress.js"></script>
 <!-- Chart.js -->
-<script src="<?php echo base_url() ?>template/vendors/Chart.js/dist/Chart.min.js"></script>
-<!-- jQuery Sparklines -->
-<script src="<?php echo base_url() ?>template/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="<?php echo site_url() ?>template/vendors/gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="<?php echo site_url() ?>template/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo site_url() ?>template/vendors/iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="<?php echo site_url() ?>template/vendors/skycons/skycons.js"></script>
 <!-- Flot -->
-<script src="<?php echo base_url() ?>template/vendors/Flot/jquery.flot.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/Flot/jquery.flot.pie.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/Flot/jquery.flot.time.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/Flot/jquery.flot.stack.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/Flot/jquery.flot.resize.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/Flot/jquery.flot.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/Flot/jquery.flot.pie.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/Flot/jquery.flot.time.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/Flot/jquery.flot.stack.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/Flot/jquery.flot.resize.js"></script>
 <!-- Flot plugins -->
-<script src="<?php echo base_url() ?>template/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/flot.curvedlines/curvedLines.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/flot.curvedlines/curvedLines.js"></script>
 <!-- DateJS -->
-<script src="<?php echo base_url() ?>template/vendors/DateJS/build/date.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="<?php echo site_url() ?>template/vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
 <!-- bootstrap-daterangepicker -->
-<script src="<?php echo base_url() ?>template/vendors/moment/min/moment.min.js"></script>
-<script src="<?php echo base_url() ?>template/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/moment/min/moment.min.js"></script>
+<script src="<?php echo site_url() ?>template/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
 
 <!-- Custom Theme Scripts -->
-<script src="<?php echo base_url() ?>template/build/js/custom.min.js"></script>
+<script src="<?php echo site_url() ?>template/build/js/custom.min.js"></script>
 
 <style>
   footer {
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="images/favicon.ico" type="image/ico" />
 
     <title>WELL TYPE </title>
-
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="<?php echo base_url() ?>template/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link href="<?php echo base_url() ?>template/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
@@ -18,7 +17,12 @@
     <link href="<?php echo base_url() ?>template/vendors/nprogress/nprogress.css" rel="stylesheet">
     <!-- bootstrap-daterangepicker -->
     <link href="<?php echo base_url() ?>template/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-
+    <!-- iCheck -->
+    <link href="<?php echo base_url() ?>template/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="<?php echo base_url() ?>template/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="<?php echo base_url() ?>template/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
     <!-- Custom Theme Style -->
     <link href="<?php echo base_url() ?>template/build/css/custom.min.css" rel="stylesheet">
     
@@ -31,24 +35,3 @@
     }
     </style>
   </head>
-  <body class="nav-md">
-        <!-- top navigation -->
-        <div class="top_nav">
-            <div class="nav_menu">
-                <div class="nav toggle">
-                  <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                </div>
-                <nav class="nav navbar-nav">
-                <ul class=" navbar-right">
-                  <li class="nav-item dropdown open" style="padding-left: 15px;">
-                    <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                      <?= $this->session->userdata('username');?>
-                    </a>
-                    <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item"  href="javascript:;"> Profile</a>
-                      <a class="dropdown-item"  href="Logout_admin"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
-                    </div>
-                </ul>
-              </nav>
-            </div>
-          </div>
