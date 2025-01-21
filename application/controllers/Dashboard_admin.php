b034a5251b3a02ec1bdd91f3f8111c22599531c1
ervinrekaa <ervinrekaa@gmail.com>
2025-01-21 15:33:27 +0700

    ervin; Dashboard Admin Card Jumlah User

--- application/controllers/Dashboard_admin.php
+++ application/controllers/Dashboard_admin.php
@@ -13,9 +13,15 @@ class Dashboard_admin extends CI_Controller {
     }
 	public function index()
 	{
+        $this->load->model('Mmember'); // Pastikan model dimuat
+
+        $data['premium_count'] = $this->Mmember->count_users_by_status('premium');
+        $data['free_count'] = $this->Mmember->count_users_by_status('free');
+        $data['total_count'] = $this->Mmember->count_total_users();
+    
         $this->load->view('template/header');
         $this->load->view('template/sidebar');
-        $this->load->view('template/index');
+        $this->load->view('template/index', $data);
 		$this->load->view('template/footer');
 	}
 }
